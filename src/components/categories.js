8d792da7a898a2c313077bec1cec6ab13c1cbee1
hadeel999 <hadeelahmadsaleh@gmail.com>
2022-09-01 23:43:46 +0300

    Modify

--- src/components/categories.js
+++ src/components/categories.js
@@ -18,6 +18,7 @@ function Categories(props) {
 
   useEffect(() => {
     props.loadCategory();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   },[]);
 
   const CategoryPick = (props) => {
